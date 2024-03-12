75bc98999c294e16dbedc0bde2441b070b80633c
Kutman_kushbaokov <147912645+kutman-05@users.noreply.github.com>
2024-03-12 17:28:39 +0600

    allProduct

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ import Admin from "./pages/Admin";
 import { useState } from "react";
 import rabbitStart from "./images/rabbit-loading.png";
 import DetalProduct from "./pages/DetalProduct/DetalProduct";
-
+import AllProduct from "./pages/AllProduct/AllProduct";
 function App() {
   const [load, setLoad] = useState(true);
   setTimeout(() => {
@@ -57,6 +57,7 @@ function App() {
         <Route path="/" element={<Home />} key={1} />
         <Route path="/admin" element={<Admin />} key={2} />
         <Route path="/detal/:id" element={<DetalProduct/>} key={3} />
+        <Route path="/allProduct" element= {<AllProduct/>} key={3}/>
       </Routes>
     </div>
   );
