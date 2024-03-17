e4e8b59ab3fc219aaad8d826ba3ea8ab1fe2689d
Arsenoverkill <arsenprogrammer007@gmail.com>
2024-03-17 16:42:51 +0600

    delete_edit

--- src/pages/Admin/index.jsx
+++ src/pages/Admin/index.jsx
@@ -1,72 +1,105 @@
 import React, { useState } from "react";
 import "./Admin.css";
-import { useDispatch } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import { ADD_PRODUCT } from "../../redux/actionTypes";
+import { useNavigate } from "react-router-dom";
 
 const Admin = () => {
   const [name, setName] = useState("");
   const [image, setImage] = useState("");
   const [price, setPrice] = useState("");
-  const [plot, setPlot] = useState("");
   const [category, setCategory] = useState("meat");
+  const navigate = useNavigate();
   const dispatch = useDispatch();
   function creatData() {
     let data = JSON.parse(localStorage.getItem("product")) || [];
-    let newObj = {
-      name,
-      image,
-      price,
-      count: 1,
-      plot,
-      category,
-      id: Date.now(),
-    };
-    data.push(newObj);
-    dispatch({ type: ADD_PRODUCT, payload: newObj });
-    localStorage.setItem("product", JSON.stringify(data));
+    if (name !== "" && image !== "" && price !== "" && category !== "") {
+      let clone = data.some((el) => {
+        return image == el.image;
+      });
+      if (clone == false) {
+        let newObj = {
+          name,
+          image,
+          price,
+          count: 1,
+          category,
+          id: Date.now(),
+        };
+        data.push(newObj);
+        dispatch({ type: ADD_PRODUCT, payload: newObj });
+        localStorage.setItem("product", JSON.stringify(data));
+        setImage("");
+        setName("");
+        setPrice("");
+        setCategory("");
+      } else {
+        alert("В магазине уже есть этот продукт !!!");
+      }
+    } else {
+      alert("Заполните все поле !!!");
+    }
+  }
+  function leaveAdmin() {
+    let password = JSON.parse(localStorage.getItem("password")) || [];
+    password[0] = false;
+    localStorage.setItem("password", JSON.stringify(password));
   }
   return (
-    <div className="admin">
-      <input
-        onChange={(e) => setName(e.target.value)}
-        class="input"
-        name="text"
-        type="text"
-        placeholder="Name..."
+    <div id="admin">
+      <img
+        src="https://fons.pibig.info/uploads/posts/2023-06/1686131064_fons-pibig-info-p-fon-dlya-produktsii-pinterest-14.jpg"
+        alt=""
+        className="fon"
       />
-      <input
-        onChange={(e) => setImage(e.target.value)}
-        class="input"
-        name="text"
-        type="text"
-        placeholder="URL..."
-      />
-      <input
-        onChange={(e) => setPrice(e.target.value)}
-        class="input"
-        name="text"
-        type="number"
-        placeholder="Price..."
-      />
-      <input
-        onChange={(e) => setPlot(e.target.value)}
-        class="input"
-        name="text"
-        type="text"
-        placeholder="Plot..."
-      />
-      <select
-        onChange={(e) => setCategory(e.target.value)}
-        id=""
-        className="select_category"
+      <button
+        onClick={() => {
+          leaveAdmin();
+          navigate("/password");
+        }}
+        className="leave"
       >
-        <option value="meat">Meat</option>
-        <option value="vegetable">Vegetable</option>
-        <option value="drink">Drink</option>
-        <option value="sweet">Sweet</option>
-        <option value="product">Product</option>
-      </select>
-      <button onClick={creatData}>CREATE</button>
+        Выйти с админа
+      </button>
+      <div className="admin">
+        <input
+          onChange={(e) => setName(e.target.value.trim())}
+          class="input"
+          name="text"
+          type="text"
+          placeholder="Name..."
+          value={name}
+        />
+        <input
+          onChange={(e) => setImage(e.target.value.trim())}
+          class="input"
+          name="text"
+          type="text"
+          placeholder="URL..."
+          value={image}
+        />
+        <input
+          onChange={(e) => setPrice(e.target.value.trim())}
+          class="input"
+          name="text"
+          type="number"
+          placeholder="Price..."
+          value={price}
+        />
+        <select
+          onChange={(e) => setCategory(e.target.value)}
+          id=""
+          className="select_category"
+          value={category}
+        >
+          <option value="meat">Meat</option>
+          <option value="vegetable">Vegetable</option>
+          <option value="drink">Drink</option>
+          <option value="sweet">Sweet</option>
+          <option value="product">Product</option>
+        </select>
+        <button onClick={creatData}>CREATE</button>
+      </div>
     </div>
   );
 };
--- src/pages/Category/Category.jsx
+++ src/pages/Category/Category.jsx
@@ -1,4 +1,5 @@
 import React, { useEffect, useState } from "react";
+import "./Category.css";
 import { useSelector } from "react-redux";
 import add_product from "../../images/add_product.svg";
 import { NavLink, useParams } from "react-router-dom";
@@ -19,22 +20,30 @@ const Category = () => {
   }, []);
   return (
     <div
+      className="container"
       style={{
         display: "flex",
         gap: "20px",
         marginTop: "70px",
       }}
     >
-      {categoryProduct.map((el) => (
-        <div className="home_block">
-          <img className="product" src={el.image} alt="" />
-          <NavLink to={`/detal/${el.id}`}>
-            <img className="add" src={add_product} alt="" />
-          </NavLink>
-          <h1>{el.price}$</h1>
-          <h3>{el.name.length > 20 ? el.name.slice(0, 20) : el.name}</h3>
-        </div>
-      ))}
+      <div className="category_blocks">
+        {categoryProduct.map((el) => (
+          <div
+            style={{
+              margin: "20px 45px",
+            }}
+            className="home_block"
+          >
+            <img className="product" src={el.image} alt="" />
+            <NavLink to={`/detal/${el.id}`}>
+              <img className="add" src={add_product} alt="" />
+            </NavLink>
+            <h1>{el.price}$</h1>
+            <h3>{el.name.length > 20 ? el.name.slice(0, 20) : el.name}</h3>
+          </div>
+        ))}
+      </div>
     </div>
   );
 };
